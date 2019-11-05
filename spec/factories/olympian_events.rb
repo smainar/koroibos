5b94ba1f2ad340fc5d309c6084153541ad12d499
Stella Mainar <mainars09@alumni.hanover.edu>
2019-11-05 08:24:56 -0700

    Edit array for enum `medal_type` in OlympianEvent factory

--- spec/factories/olympian_events.rb
+++ spec/factories/olympian_events.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :olympian_event do
-    medal_type = ['NA', 'gold', 'silver', 'bronze']
-    
+    medal_type = ['NA', 'Gold', 'Silver', 'Bronze']
+
     medal { medal_type.sample }
     olympian { Faker::Name.name }
     event { Faker::Esport.event }
